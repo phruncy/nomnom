b2025686f6012ad397fa2d228b83855d26ff1f98
Franziska Schneider <franziska.schneider@me.com>
2025-06-03 22:29:32 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
   title = 'nomnom';
