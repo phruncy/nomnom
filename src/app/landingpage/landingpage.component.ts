0eda7114d180e69b762295ec1b7797d34c0c8b3f
Franziska Schneider <franziska.schneider@me.com>
2025-06-09 09:49:44 +0200

    formatting

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
-import { RecipeWidgetDisplayComponent } from "../recipe-widget-display/recipe-widget-display.component";
+import { RecipeWidgetDisplayComponent } from '../recipe-widget-display/recipe-widget-display.component';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-landingpage',
-  imports: [ RecipeWidgetDisplayComponent],
-  template: `
-    <header>
-      <h1>CHOMP CHOMP CHOMP</h1>
-    </header>
-    <main>
-      <app-recipe-widget-display></app-recipe-widget-display>
-    </main>
-    <footer></footer>
-  `,
-  styleUrl: './landingpage.component.scss'
+    selector: 'app-landingpage',
+    imports: [RecipeWidgetDisplayComponent, RouterLink, MatButtonModule],
+    template: `
+        <header>
+            <h1>CHOMP CHOMP CHOMP</h1>
+        </header>
+        <main>
+            <app-recipe-widget-display></app-recipe-widget-display>
+            <a matButton routerLink="create">New Recipe</a>
+        </main>
+        <footer></footer>
+    `,
+    styleUrl: './landingpage.component.scss',
 })
-export class LandingpageComponent {
-
-}
+export class LandingpageComponent {}
