f97684eb64c0dbddc27597e0cd55dc30e0b1ac7a
Franziska Schneider <franz@Freund-Rechner.local>
2025-06-10 17:56:10 +0200

    Add global footer component

---
+++ src/app/layout/global/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-footer',
+    imports: [],
+    template: `
+        <div class="container"></div>
+        <p class="content">Footer</p>
+    `,
+    styleUrl: './footer.component.scss',
+})
+export class FooterComponent {}
