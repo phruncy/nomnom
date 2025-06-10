43825fde991a8c98309f4857a1fe9050dd108af5
Franziska Schneider <franz@Freund-Rechner.local>
2025-06-10 17:18:54 +0200

    Move layouts subfolder

---
+++ src/app/layout/create-recipe/create-recipe.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-recipe',
+  imports: [],
+  template: `
+    <p>
+      create-recipe works!
+    </p>
+  `,
+  styleUrl: './create-recipe.component.scss'
+})
+export class CreateRecipeComponent {
+
+}
